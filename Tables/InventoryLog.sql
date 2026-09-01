4723b0d783889db7f8eeba28c549aedba6382b3f
Firas <tarmoun@msn.com>
2026-09-01 08:44:20 -0700

    Add Notes column to InventoryLog table

--- Tables/InventoryLog.sql
+++ Tables/InventoryLog.sql
@@ -3,5 +3,6 @@ CREATE TABLE [dbo].[InventoryLog] (
     [ProductID] INT NOT NULL,
     [ChangeDate] DATETIME2 NOT NULL DEFAULT GETDATE(),
     [QuantityChange] INT NOT NULL,
-    [ChangeType] NVARCHAR(20) NOT NULL
+    [ChangeType] NVARCHAR(20) NOT NULL,
+    COMMENTS TEXT
 );
